3774610e83865a882b23f4044b106f7fc7f56f4c
xiong <592802622@qq.com>
2023-04-17 02:26:29 +0800

    处理下支付回调的问题

--- src/main/java/club/doctorxiong/api/service/OrderService.java
+++ src/main/java/club/doctorxiong/api/service/OrderService.java
@@ -106,9 +106,8 @@ public class OrderService {
             return;
         } else {
             //更新订单信息
-            currentOrder.setPaySuccess(true);
             finishTokenOrder(currentOrder);
-            orderRequestCache.invalidate(result.getMoney());
+            currentOrder.setPaySuccess(true);
         }
     }
 
